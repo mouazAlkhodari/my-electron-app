f7bb3919d036b46f693886b63247e2f58d1c1a3a
mouaz.alkhodari <mouazx19999@gmail.com>
2023-03-14 19:20:35 +0300

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const { app, BrowserWindow, ipcMain } = require("electron");
+require('update-electron-app')()
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  ipcMain.handle("ping", () => "pong");
+  
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
